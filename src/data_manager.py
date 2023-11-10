8d4520c8c95606a68494b3cc20740beff07f25bd
Alex <amcpherson@live.ca>
2023-11-10 00:09:32 -0400

    Generalized data_manager for use with settings data

--- src/data_manager.py
+++ src/data_manager.py
@@ -3,50 +3,78 @@ import os
 
 
 # Import and export user settings and profiles via json files in the src/data folder
-class DataManager:
-    def __init__(self, file_path="profiles.json"):
-        self.file_path = file_path
-        self.profiles = self.load_profiles()
+class BaseManager:
 
-    def load_profiles(self):
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.data = self.load_data()
+
+    def load_data(self):
         if os.path.exists(self.file_path):
             with open(self.file_path, "r") as json_file:
                 return json.load(json_file)
         else:
             return {}
 
-    def save_profiles(self):
+    def save_data(self):
+        os.makedirs(os.path.dirname(self.file_path), exist_ok=True)
         with open(self.file_path, "w") as json_file:
-            json.dump(self.profiles, json_file, indent=2)
+            json.dump(self.data, json_file, indent=2)
+
+    def add_entry(self, key, value):
+        if key not in self.data:
+            self.data[key] = value
+            self.save_data()
+            return True
+        else:
+            return False  # Entry with the same key already exists
+
+    def remove_entry(self, key):
+        if key in self.data:
+            del self.data[key]
+            self.save_data()
+            return True
+        else:
+            return False  # Entry with the given key doesn't exist
+
+    def update_entry(self, key, value):
+        if key in self.data:
+            self.data[key] = value
+            self.save_data()
+            return True
+        else:
+            return False  # Entry with the given key doesn't exist
+
+
+class ProfileManager(BaseManager):
+
+    def __init__(self, file_path="data/profiles.json"):
+        super().__init__(file_path)
 
     def add_profile(self, profile_name):
-        if profile_name not in self.profiles:
-            self.profiles[profile_name] = []
-            self.save_profiles()
-            return True
-        else:
-            return False  # Profile with the same name already exists
+        return self.add_entry(key=profile_name, value=[])
 
     def remove_profile(self, profile_name):
-        if profile_name in self.profiles:
-            del self.profiles[profile_name]
-            self.save_profiles()
-            return True
-        else:
-            return False  # Profile with the given name doesn't exist
+        return self.remove_entry(key=profile_name)
 
     def add_path_to_profile(self, profile_name, path):
-        if profile_name in self.profiles and path not in self.profiles[profile_name]:
-            self.profiles[profile_name].append(path)
-            self.save_profiles()
+        if profile_name in self.data and path not in self.data[profile_name]:
+            self.data[profile_name].append(path)
+            self.save_data()
             return True
         else:
             return False  # Profile with the given name doesn't exist
 
     def remove_path_from_profile(self, profile_name, path):
-        if profile_name in self.profiles and path in self.profiles[profile_name]:
-            self.profiles[profile_name].remove(path)
-            self.save_profiles()
+        if profile_name in self.data and path in self.data[profile_name]:
+            self.data[profile_name].remove(path)
+            self.save_data()
             return True
         else:
             return False  # Profile or path doesn't exist
+
+
+class SettingsManager(BaseManager):
+
+    def __init__(self, file_path="settings.json"):
+        super().__init__(file_path)
--- test/test_data_manager.py
+++ test/test_data_manager.py
@@ -1,27 +1,30 @@
 import unittest
-import json
 import os
 
-from src.data_manager import DataManager
+from src.data_manager import ProfileManager
+from src.data_manager import SettingsManager
 
 
 class TestDataManager(unittest.TestCase):
 
     def setUp(self):
-        # Create a temporary file path for testing
-        self.test_file_path = "test_profiles.json"
-        # Create a ProfileManager instance for testing
-        self.profile_manager = DataManager(self.test_file_path)
+        self.test_profiles_file_path = "data/test_profiles.json"
+        self.test_settings_file_path = "data/test_settings.json"
+
+        self.profile_manager = ProfileManager(file_path=self.test_profiles_file_path)
+        self.settings_manager = SettingsManager(file_path=self.test_settings_file_path)
 
     def tearDown(self):
-        # Remove the temporary file after testing
-        if os.path.exists(self.test_file_path):
-            os.remove(self.test_file_path)
+        # Remove the temporary files after testing
+        if os.path.exists(self.test_profiles_file_path):
+            os.remove(self.test_profiles_file_path)
+        if os.path.exists(self.test_settings_file_path):
+            os.remove(self.test_settings_file_path)
 
     def test_add_profile(self):
         # Adding a profile should return True
         self.assertTrue(self.profile_manager.add_profile("TestProfile"))
-        self.assertEqual(self.profile_manager.profiles["TestProfile"], [])
+        self.assertEqual(self.profile_manager.data["TestProfile"], [])
 
         # Adding the same profile again should return False
         self.assertFalse(self.profile_manager.add_profile("TestProfile"))
@@ -31,7 +34,7 @@ class TestDataManager(unittest.TestCase):
 
         # Removing a profile should return True
         self.assertTrue(self.profile_manager.remove_profile("TestProfile"))
-        self.assertNotIn("TestProfile", self.profile_manager.profiles)
+        self.assertNotIn("TestProfile", self.profile_manager.data)
 
         # Removing a non-existent profile should return False
         self.assertFalse(self.profile_manager.remove_profile("NonExistentProfile"))
@@ -41,7 +44,7 @@ class TestDataManager(unittest.TestCase):
 
         # Adding a path to a profile should return True
         self.assertTrue(self.profile_manager.add_path_to_profile("TestProfile", "C:/test/file.exe"))
-        self.assertIn("C:/test/file.exe", self.profile_manager.profiles["TestProfile"])
+        self.assertIn("C:/test/file.exe", self.profile_manager.data["TestProfile"])
 
         # Adding the same path to the same profile should return False
         self.assertFalse(self.profile_manager.add_path_to_profile("TestProfile", "C:/test/file.exe"))
@@ -55,13 +58,35 @@ class TestDataManager(unittest.TestCase):
 
         # Removing a path from a profile should return True
         self.assertTrue(self.profile_manager.remove_path_from_profile("TestProfile", "C:/test/file.exe"))
-        self.assertNotIn("C:/test/file.exe", self.profile_manager.profiles["TestProfile"])
+        self.assertNotIn("C:/test/file.exe", self.profile_manager.data["TestProfile"])
 
         # Removing a non-existent path from a profile should return False
         self.assertFalse(self.profile_manager.remove_path_from_profile("TestProfile", "NonExistentPath"))
 
         # Removing a path from a non-existent profile should return False
         self.assertFalse(self.profile_manager.remove_path_from_profile("NonExistentProfile", "C:/test/file.exe"))
+
+    def test_add_setting(self):
+        self.assertTrue(self.settings_manager.add_entry("Setting 1", True))
+        self.assertEqual(self.settings_manager.data["Setting 1"], True)
+
+        # Adding the same setting again should return False
+        self.assertFalse(self.settings_manager.add_entry("Setting 1", "value2"))
+        self.assertFalse(self.settings_manager.add_entry("Setting 1", True))
+
+    def test_remove_setting(self):
+        self.assertFalse(self.settings_manager.remove_entry("Setting 1"))
+
+        self.settings_manager.add_entry("Setting 1", "value1")
+        self.assertTrue(self.settings_manager.remove_entry("Setting 1"))
+        self.assertNotIn("Setting 1", self.settings_manager.data)
+
+    def test_update_setting(self):
+        self.assertFalse(self.settings_manager.update_entry("Setting 1", "value1"))
+
+        self.settings_manager.add_entry("Setting 1", "value1")
+        self.assertTrue(self.settings_manager.update_entry("Setting 1", "value2"))
+        self.assertEqual(self.settings_manager.data["Setting 1"], "value2")
 
 
 if __name__ == '__main__':
